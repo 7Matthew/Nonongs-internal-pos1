1b4d550da2496a929856b27e5cc4020c1a193952
airavirtusio <airadynielle7@gmail.com>
2022-09-26 16:36:49 +0800

    latest commit-aira

--- resources/views/admin/food-menu-crud/create.blade.php
+++ resources/views/admin/food-menu-crud/create.blade.php
@@ -8,30 +8,31 @@
     <div class="card-header">New Item</div>
     <div class="card-body">
        
-        <form action="{{ route('food-item.store') }}" method="post">
+        <form action="{{ route('food-item.store') }}" method="post" enctype="multipart/form-data">
           @csrf
-          <label>Name</label></br>
+          <label for="name">Name</label></br>
           <input type="text" name="name" id="name" class="form-control" value ="{{old('name')}}"></br>
           @error('name')
             <div class="alert alert-danger" role="alert">
               <i class="fa-solid fa-circle-exclamation"></i>{{ucwords($message)}}
             </div>
           @enderror
-          <label>Price</label></br>
+          <label for="price">Price</label></br>
           <input type="number" name="price" id="price" class="form-control" value ="{{old('price')}}"></br>
           @error('price')
             <div class="alert alert-danger" role="alert">
               <i class="fa-solid fa-circle-exclamation"></i>{{ucwords($message)}}
             </div>
           @enderror
-          <label>Stocks</label></br>
+          <label for="stocks">Stocks</label></br>
           <input type="number" name="stocks" id="stocks" class="form-control" value ="{{old('stocks')}}"></br>
           @error('stocks')
             <div class="alert alert-danger" role="alert">
               <i class="fa-solid fa-circle-exclamation"></i>{{ucwords($message)}}
             </div>
           @enderror
-          <input type="submit" value="Save" class="btn btn-success"></br>
+          
+          <input type="submit" value="Save" class="btn btn-success mt-2"></br>
       </form>
       
     </div>
